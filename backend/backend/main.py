df355d035cb60e8a38f25da56508d89e07ab4d07
Carine Casagrande <carine.casagrande@hotmail.com>
2021-11-25 22:57:19 -0300

    .

--- backend/backend/main.py
+++ backend/backend/main.py
@@ -1,16 +1,13 @@
 from typing import List
-
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
-
-
 from . import crud, models, schemas
 from .database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
-    
+
 app = FastAPI()
-    
+
 def get_db():
     db = SessionLocal()
     try:
@@ -18,6 +15,7 @@ def get_db():
     finally:
         db.close()
 
+# Cria usuário
 @app.post("/users/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
@@ -25,15 +23,21 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
         raise HTTPException(status_code=400, detail="E-mail já cadastrado")
 
     user = crud.create_user(db=db, user=user)
-    
+
     return user
 
+# Busca os usuários
 @app.get("/users/", response_model=List[schemas.User])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
 
-#GET de usuário por ID (/users/<int:id>/)
+# Busca um determinado usuário
 @app.get("/users/{id}", response_model=schemas.User)
 def get_user(id: int, db: Session = Depends(get_db)):
     return crud.get_user(db, id)
+
+# Alteração de usuário
+@app.put("/users/{user_id}", response_model=schemas.User)
+async def edit_user(id: int, new_value: schemas.UserEdit, db: Session = Depends(get_db)):
+    crud.edit_user(db, id, new_value=new_value)
